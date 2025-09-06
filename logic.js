237e1191ceb7eaa51a9e57ac55abbd5df94204fc
WageRager <135900565+astockmanTect@users.noreply.github.com>
2025-09-05 22:52:00 -0500

    Initial Commit for task manager project

---
+++ logic.js
@@ -0,0 +1,415 @@
+const textItem = document.getElementById("taskInput");
+const taskTableBody = document.getElementById("taskTableBody");
+const taskTable = document.getElementById("taskTable");
+let dataLoaded = false;
+let validation = false;
+localStorage.getItem("objectCountStorage");
+let task;
+
+baseURL = "INSERT BASE URL";
+
+// retrieve data from the database
+async function getData() {
+    const url = baseURL + "/tasks";
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+  
+      const result = await response.json();
+      populateAPIData(result);
+      console.log(result);
+    } catch (error) {
+      console.error(error.message);
+    }
+}
+
+function populateAPIData(result){
+    // console.log(result[0]);
+    if (dataLoaded == true) {
+        alert('Data Already Loaded!');
+        return;
+    }
+    else {
+        Object.values(result).forEach(value => {
+            console.log(value);
+            console.log(value.ID);
+            // console.log(value.TASKS);
+            // console.log(value.STATUS);
+            // console.log(value.DONE);
+            saveNewRowAPI(value);
+            dataLoaded = true
+        })
+    }
+    // console.log(jsObjects);
+    return;
+}
+
+async function updateCheckBox() {
+    return;
+}
+
+async function updateRecord(field, value, ID) {
+    const parentRow = element.closest(".tableRow");
+    const parentRowElement = parentRow.id
+    const parentRowID = parentRowElement.substring(6);
+    const url = baseURL + "/removeSingleTask/" + parentRowID;
+
+    const updateData = {
+        field: field,
+        value: value,
+        ID: ID
+    }
+
+    try {
+        const response = await fetch(url, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: updateData
+        });
+
+        if (!response.ok){
+            const errorData = await response.json();
+            throw new Error(`Server error: ${response.status} - ${errorData.error}`);s
+        }
+    }catch(error){
+        console.error(error.message);
+    }
+
+}
+
+async function deleteRecord(element){
+    const parentRow = element.closest(".tableRow");
+    const parentRowElement = parentRow.id
+    const parentRowID = parentRowElement.substring(6);
+    const url = baseURL + "/removeSingleTask/" + parentRowID;
+
+    try {
+        const response = await fetch(url, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        });
+    
+        if (!response.ok) {
+            const errorData = await response.json();
+            throw new Error(`Server error: ${response.status} - ${errorData.error}`);
+        }
+
+        const result = await response.json();
+        console.log(result);
+        parentRow.remove();
+        // this.closest("tr").remove(); // removes nearest row element
+        // this.parentElement.remove(); removes parent element, which would be the <td>
+    } catch (error) {
+      console.error(error.message);
+    }
+}
+
+async function deleteAllRecords(){
+    
+    const url = baseURL + "/removeTasks";
+    try {
+      console.log(url);
+      const response = await fetch(url, {
+        method: "DELETE"
+      });
+
+      // Check for successful response
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+
+      const result = await response.json();
+      console.log(result);
+
+      location.reload();
+    } catch (error) {
+      console.error(error.message);
+    }
+}
+
+function createValAlert(mainElement, text){
+    let alertDiv = document.createElement("div")
+    alertDiv.setAttribute("class", "formAlert");
+
+    let alertText = document.createTextNode(text);
+
+    alertDiv.appendChild(alertText);
+    return alertDiv;
+}
+
+// Add data to the database
+async function postData() {
+
+    if(textItem.value === '') {
+        if (validation === false) {
+            const mainForm = document.getElementsByClassName("mainForm")[0];
+            let alertDiv = createValAlert(mainForm, "Form Data Empty: Please Input a Task Name!");
+            mainForm.appendChild(alertDiv);
+            validation = true;
+        }
+        else {
+            console.log("validation already hit")
+        }
+    }
+    else {
+        const url = baseURL + "/add";
+
+        const newTask = {
+            task: textItem.value,
+            status: 'Not Done',
+            done: 0
+        }
+
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(newTask)
+            });
+        
+            if (!response.ok) {
+                const errorData = await response.json();
+                throw new Error(`Server error: ${response.status} - ${errorData.error}`);
+            }
+
+            const result = await response.json();
+            console.log(result);
+
+            location.reload();
+        } catch (error) {
+        console.error(error.message);
+        }
+    }
+}
+
+
+async function createTestData(){
+    const url = baseURL + "/add";
+
+    for (let i = 0; i < 100; i++){
+        const newTask = {
+            task: 'test-' + Math.round(Math.random() * 100000) ,
+            status: 'Not Done',
+            done: 0
+        }
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(newTask)
+            });
+        
+            if (!response.ok) {
+                const errorData = await response.json();
+                throw new Error(`Server error: ${response.status} - ${errorData.error}`);
+            }
+    
+            const result = await response.json();
+            console.log(result);
+        } catch (error) {
+          console.error(error.message);
+        }
+    }
+    return(location.reload());
+}
+
+// Main handler for creating tables from API data
+function saveNewRowAPI(result) {
+    let newRow = createNewTableRowAPI(result);
+    newRow.className = 'tableRow';
+    taskTableBody.appendChild(newRow);
+}
+
+function createNewTableRowAPI(result) {
+    let newRow = document.createElement("tr");
+    newRow.id = "Record" + result.ID;
+
+    let newcell = document.createElement("td");
+    newcell.id = "cell-" + result.ID + "-1";
+    newcell.className = "newCell";
+
+    let newcell2 = document.createElement("td");
+    newcell2.id = "cell-" + result.ID + "-2";
+    newcell2.className = "newCell";
+
+    let newcell3 = document.createElement("td");
+    newcell3.id = "cell-" + result.ID + "-3";
+    newcell3.className = "newCell";
+
+    let newcell4 = document.createElement("td");
+    newcell4.id = "cell-" + result.ID + "-4";
+    newcell4.className = "newCell";
+
+    setNewCellValuesAPI(result, newcell, newcell2, newcell3, newcell4);
+    appendNewCells(newRow, newcell, newcell2, newcell3, newcell4);
+    return newRow;
+}
+
+function appendNewCells(newRow, newcell, newcell2, newcell3, newcell4) 
+{
+    console.log("Appending new cells");
+    newRow.appendChild(newcell);
+    newRow.appendChild(newcell2);
+    newRow.appendChild(newcell3);
+    newRow.appendChild(newcell4);
+    return 'true';
+}
+
+function setNewCellValuesAPI(result, newcell, newcell2, newcell3, newcell4) {
+    newcell.textContent = result.TASKS;
+    newcell2.textContent = result.STATUS;
+
+    const checkbox = document.createElement("input");
+    checkbox.setAttribute("type", "checkbox");
+    checkbox.setAttribute("class", "taskCheckBox");
+    newcell3.appendChild(checkbox);
+
+    let deleteButton = createDeleteButton("Record" + result.ID);
+    newcell4.appendChild(deleteButton);
+
+    return 'true';
+}
+
+function createDeleteButton(recordId){
+
+    let deleteButton = document.createElement("button");
+    deleteButton.setAttribute("class", "deleteRecord");
+    deleteButton.setAttribute("onclick", "deleteRecord(this)")
+    deleteButton.textContent = "Delete";
+
+    return deleteButton;
+}
+
+// let objectCountStorage;
+// if (localStorage.getItem("objectCountStorage") == null) {
+//     objectCountStorage = 0;
+// }
+// else {
+//     objectCountStorage = parseInt(localStorage.getItem("objectCountStorage"));
+// }
+
+// function getDataOnLoad() {
+//     console.log('Getting Data');
+//     for (let i = 0; i < localStorage.length; i++){
+//         const key = localStorage.key(i);
+//         if (key && key.startsWith("Record")) {
+//             let record = JSON.parse(localStorage.getItem(key));
+//             if (record && record.Task){
+//                 let newRow = createNewTableRow(objectCountStorage, false, false, empty);
+//                 newRow.className = 'tableRow';
+//                 taskTableBody.appendChild(newRow);
+//                 console.log("Row created from local");
+//             }
+//             else {
+//                 console.log('record not valid');
+//             }
+//         }
+//     }
+// }
+
+// Set the value of the text area box, if there's a value in local storage
+function setTaskInput() {
+    task = textItem.value;
+}
+
+// function saveNewRow() {
+//     if (textItem.value === null)
+//     {
+//         console.log("No text to save");
+//     } 
+//     else if (textItem.value.length > 0) {
+//         // setting metrics for row counting and current task item
+//         objectCountStorage += 1
+//         localStorage.setItem("objectCountStorage", objectCountStorage);
+//         localStorage.setItem("task", textItem.value);
+
+//         let newRow = createNewTableRow(objectCountStorage, true, false, empty);
+//         newRow.className = 'tableRow';
+
+//         taskTableBody.appendChild(newRow);
+
+
+//         console.log("Object count: " + objectCountStorage);
+        
+//         console.log("updated");
+//     } else {
+//         console.log("No text to save"); 
+//     }
+// }
+
+// function createNewTableRow(objectCountStorage, newRecord, APIRecord, result)
+// {
+//         let newRow = document.createElement("tr");
+//         newRow.id = "Record" + objectCountStorage;
+
+//         let newcell = document.createElement("td");
+//         newcell.id = "cell-" + objectCountStorage + "-1";
+//         newcell.className = "newCell";
+
+//         let newcell2 = document.createElement("td");
+//         newcell2.id = "cell-" + objectCountStorage + "-2";
+//         newcell2.className = "newCell";
+
+//         let newcell3 = document.createElement("td");
+//         newcell3.id = "cell-" + objectCountStorage + "-3";
+//         newcell3.className = "newCell";
+
+//         let newcell4 = document.createElement("td");
+//         newcell4.id = "cell-" + objectCountStorage + "-4";
+//         newcell4.className = "newCell";
+
+//         if (APIRecord == true){
+
+//         }
+//         else {
+//             setNewCellValues(newcell, newcell2, newcell3, newcell4, objectCountStorage);
+//         }
+
+//         appendNewCells(newRow, newcell, newcell2, newcell3, newcell4);
+//         if (newRecord == true) {
+//             setNewLocalRecord(newRow, newcell, newcell2, newcell3, newcell4, objectCountStorage)
+//             console.log("New record");
+//         }
+//         else {
+//             console.log("Old record");
+//         }
+//         return newRow;
+// }
+
+// function setNewCellValues(newCell, newcell2, newcell3, newcell4, objectCountStorage) 
+// {
+//     newCell.textContent = textItem.value;
+//     newcell2.textContent = Date.UTC().toString();
+//     newcell3.appendChild(document.createElement("input")).setAttribute("type", "checkbox");
+
+//     let deleteButton = createDeleteButton("Record" + objectCountStorage);
+//     newcell4.appendChild(deleteButton);
+
+//     return 'true';
+// }
+
+function setNewLocalRecord(newRow, newcell, newcell2, newcell3, newcell4, objectCount)
+{
+    const newRecord = 
+    {
+        row: objectCount,
+        Task: newcell.textContent,
+        Status: newcell2.textContent,
+        Done: null,
+        Delete: null
+    }
+
+    const newRecordAsString = JSON.stringify(newRecord);
+    localStorage.setItem('Record' + objectCount, newRecordAsString);
+}
+
---
+++ server.js
@@ -0,0 +1,142 @@
+import http, { ServerResponse } from "http";
+import Database from "better-sqlite3";
+
+const db = new Database('Database.db');
+import dotenv from 'dotenv';
+dotenv.config();
+const baseURL = process.env.baseURL;
+const backendPort = process.env.backendPort;
+
+// Example: ensure a table exists
+db.prepare(`
+  CREATE TABLE IF NOT EXISTS tasks (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    completed INTEGER DEFAULT 0
+  )
+`).run();
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  res.setHeader("Access-Control-Allow-Methods", "*");
+
+  const {method, url} = req;
+
+  const segmentURL = new URL(req.url, `http://${req.headers.host}`);
+  // Split the path to extract segments
+  const pathSegments = segmentURL.pathname.split('/').filter(segment => segment.length > 0);
+  // res.end("Hello World\n");
+
+  // restful APIs have a method (i.e. GET, POST, etc.) and a URL
+  if (url === "/tasks" && method === 'GET') {
+
+    // Read all tasks
+    const rows = db.prepare("SELECT * FROM tasks").all();
+    res.end(JSON.stringify(rows));
+
+  } 
+  else if (url === "/add" && method === 'POST') {
+    // Add a sample task
+    // const stmt = db.prepare("INSERT INTO tasks (TASKS) VALUES (?)");
+    // const info = stmt.run("Sample task");
+
+    let body = '';
+    req.on('data', (chunk) => {
+      body += chunk.toString(); // convert buffer to string
+    });
+
+    req.on('end', () => {
+      try {
+        const {task, status, done } = JSON.parse(body);
+      
+        if(!task || !status) {
+          res.statusCode = 400;
+          res.end(JSON.stringify({error: "Missing required fields: task and status"}));
+          return;
+        }
+        const stmt = db.prepare("INSERT INTO tasks (TASKS, STATUS, DONE) VALUES (?,?,?)");
+        const info = stmt.run(task, status, done ? 1: 0);
+
+        res.statusCode = 201;
+        res.end(JSON.stringify({ id: info.lastInsertRowid, message: "Task added successfully" }));
+      } catch(error){
+        res.statusCode = 400;
+        res.end(JSON.stringify({ error: "Invalid JSON or bad request", info: error.all}));
+      }
+    });
+  } 
+  else if (url === '/removeTasks' && method === 'DELETE'){
+    try {
+      const stmt = db.prepare("DELETE FROM tasks");
+      stmt.run();
+
+      res.end(JSON.stringify("All Data removed from tasks table"));
+      res.statusCode = 201;
+    }catch(error){
+      res.statusCode = 400;
+      res.end(JSON.stringify({ error: "Invalid JSON or bad request", info: error.all}));
+    }
+  } 
+  else if (pathSegments[0] === 'removeSingleTask' && pathSegments.length === 2 && method === 'DELETE') {
+    
+    const IDNumber = pathSegments[1];
+    if (!IDNumber) {
+      res.statusCode = 400;
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify({ error: 'Task ID not provided.' }));
+      return; // End execution
+    }
+
+    try {
+      const stmt = db.prepare("DELETE FROM tasks WHERE ID = ?");
+      const info = stmt.run(IDNumber);
+
+      res.end(JSON.stringify(IDNumber + ": Record removed from tasks table"));
+      res.statusCode = 201;
+      
+    }catch(error){
+      res.statusCode = 401;
+      res.end(JSON.stringify({ error: "Invalid JSON or bad request", info: error.all}));
+    }
+  }
+  else if (url ==='/change' && method === "PUT"){
+  
+    let body = '';
+    req.on('data', (chunk) => {
+      body += chunk.toString(); // convert buffer to string
+    });
+
+    req.on('end', () =>{
+      try {
+        const {field, value, ID} = JSON.parse(body);
+      
+        if(!field || !value || !ID) {
+          res.statusCode = 400;
+          res.end(JSON.stringify({error: "Missing required fields: task and status"}));
+          return;
+        }
+        const stmt = db.prepare("UPDATE tasks SET ? = ? WHERE ID = ?")
+        const info = stmt.run(field, value, ID);
+
+        res.statusCode = 201;
+        res.end(JSON.stringify({ id: info.lastInsertRowid, message: "Task updated successfully" }));
+      } catch(error){
+        res.statusCode = 400;
+        res.end(JSON.stringify({ error: "Invalid JSON or bad request", info: error.all}));
+      }
+    });
+  }
+  else {
+
+    res.end(JSON.stringify({ message: "Hello World" }));
+
+  }
+});
+
+const PORT = backendPort;
+server.listen(PORT, () => {
+  console.log(`Server running at ${baseURL}:${PORT}`);
+});
