693b966357c54f2e08601987a2689da9fff36957
TheSirDingle <deathawk1998@gmail.com>
2025-09-23 20:46:28 -0500

    Added a random draggable element that you... you guesesd it, DRAG

--- Frontend/logic.js
+++ Frontend/logic.js
@@ -396,6 +396,42 @@ sidebarButton.addEventListener('click', () => {
     
 });
 
+dragElement(document.getElementById('bsDiv'));
+
+function dragElement(elm) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    elm.onmousedown = dragMouseDown;
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        pos3 = e.clientX;
+        pos4= e.clientY;
+        document.onmouseup = closeDragElement;
+        document.onmousemove = elementDrag;
+    }
+    
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        elm.style.top = (elm.offsetTop - pos2) + "px";
+        elm.style.left = (elm.offsetLeft - pos1) + "px";
+      }
+    
+    function closeDragElement() {
+        /* stop moving when mouse button is released:*/
+        document.onmouseup = null;
+        document.onmousemove = null;
+      }
+}
+
+
 // let objectCountStorage;
 // if (localStorage.getItem("objectCountStorage") == null) {
 //     objectCountStorage = 0;
